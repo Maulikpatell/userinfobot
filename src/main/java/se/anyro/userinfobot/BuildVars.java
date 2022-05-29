b9d25534f2cef7e82a3f57827fa19d45d32b8b32
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-05-30 04:27:29 +0530

    Update BuildVars.java

--- src/main/java/se/anyro/userinfobot/BuildVars.java
+++ src/main/java/se/anyro/userinfobot/BuildVars.java
@@ -1,6 +1,6 @@
 package se.anyro.userinfobot;
 
 public class BuildVars {
-    public static final int OWNER = // Your user id here. Send message to @userinfobot to get it.
-    public static final String TOKEN = // Your bot token here. Use @BotFather.
+    public static final int OWNER = 788277212// Your user id here. Send message to @userinfobot to get it.
+    public static final String TOKEN =5315983483:AAFyUYWLA1cOBD80P27GnU5BQIX9BZrvEcU // Your bot token here. Use @BotFather.
 }
